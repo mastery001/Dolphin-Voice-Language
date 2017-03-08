060dace8077c979d3e602717fc139690aab15d92
“zouziwen” <zouziwen@le.com>
2017-03-08 16:11:36 +0800

    update

--- src/main/java/dv/bootstrap/Bootstrap.java
+++ src/main/java/dv/bootstrap/Bootstrap.java
@@ -7,7 +7,7 @@ import dv.Compiler;
 public class Bootstrap {
 	
 	public static void main(String[] args) throws IOException {
-		String filename = "E:/practice/study/study-compiler-theory/src/main/resources/test.dv";
+		String filename = "src/main/resources/test.dv";
 		args = new String[]{filename};
 		Compiler compiler = new Compiler();
 		compiler.start(args);
